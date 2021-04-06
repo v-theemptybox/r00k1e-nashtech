9615583ce94b2329d57aa43e8f24ee29c48872fb
Vinh Nguyen <vinhnguyen.goid2@gmail.com>
2021-04-06 14:28:32 +0700

    Fix logout flow

---
+++ ECommerce.Backend/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ECommerce.Backend.Data;
+using ECommerce.Backend.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ECommerce.Backend.Areas.Identity.IdentityHostingStartup))]
+namespace ECommerce.Backend.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
