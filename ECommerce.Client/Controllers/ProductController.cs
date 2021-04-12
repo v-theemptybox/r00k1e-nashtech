59d3f2fd63d928652fc287225ad8002c58188868
Vinh Nguyen <vinhnguyen.goid2@gmail.com>
2021-04-12 23:44:14 +0700

    Update UI

--- ECommerce.Client/Controllers/ProductController.cs
+++ ECommerce.Client/Controllers/ProductController.cs
@@ -1,9 +1,6 @@
 ﻿using ECommerce.Client.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace ECommerce.Client.Controllers
--- ECommerce.Client/Startup.cs
+++ ECommerce.Client/Startup.cs
@@ -2,15 +2,11 @@ using ECommerce.Client.Services.APIs;
 using ECommerce.Client.Services.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace ECommerce.Client
 {
