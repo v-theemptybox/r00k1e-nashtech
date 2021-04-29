d653fd542b3582eeb63b648d5dd2304f7d41bcb4
Vinh Nguyen <vinhnguyen.goid2@gmail.com>
2021-04-29 13:51:38 +0700

    Add login view and account.service(react)

--- ecommerce-react-admin/src/Components/Header.js
+++ ecommerce-react-admin/src/Components/Header.js
@@ -1,16 +1,53 @@
 import React, { useState } from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input } from 'reactstrap';
+
 const Example = (props) => {
   const [collapsed, setCollapsed] = useState(true);
-
   const toggleNavbar = () => setCollapsed(!collapsed);
+
+  const {
+    buttonLabel,
+    className
+  } = props;
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
+
+  const btnLogin = () =>{
+    let email = document.getElementById("txtEmail").value;
+    let password = document.getElementById("txtPwd").value;
+  }
 
   return (
     <div>
       <Navbar color="faded" light>
         <NavbarBrand href="/" className="mr-auto">Admin.Site</NavbarBrand>
         <Nav>
-        <NavLink href="/category">Categories</NavLink> <NavLink href="/products">Products</NavLink><NavLink href="/users">Users</NavLink>
+          <Button color="success" onClick={toggle}>{buttonLabel}Login</Button>
+          <Modal isOpen={modal} toggle={toggle} className={className}>
+            <ModalHeader toggle={toggle}>V-eshop Login</ModalHeader>
+            <ModalBody>
+              <Form>
+              <FormGroup>
+                <Label for="exampleEmail">Email</Label>
+                <Input type="email" name="email" id="txtEmail" placeholder="example@mail.com" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="examplePassword">Password</Label>
+                <Input type="password" name="password" id="txtPwd" placeholder="Ex@mp1e" />
+              </FormGroup>
+              </Form>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={toggle}>Login</Button>{' '}
+              <Button color="secondary" onClick={toggle}>Cancel</Button>
+            </ModalFooter>
+          </Modal>
+
+          <NavLink href="/category">Categories</NavLink>
+          <NavLink href="/products">Products</NavLink>
+          <NavLink href="/users">Users</NavLink>
+
         </Nav>
         <NavbarToggler onClick={toggleNavbar} className="mr-2" />
         <Collapse isOpen={!collapsed} navbar>
---
+++ ecommerce-react-admin/src/services/account.service.js
@@ -0,0 +1,30 @@
+import { api } from '../utilities/api.config';
+
+const LoginService = (username, password) =>{
+    var urlencoded = new URLSearchParams();
+        urlencoded.append("grant_type", "password");
+        urlencoded.append("username", username);
+        urlencoded.append("password", password);
+        urlencoded.append("client_id", "react_admin");
+        urlencoded.append("client_secret", "secret");
+        
+    return await api.post('/connect/token', urlencoded)
+            .then(response => {
+                return response.data;
+            })
+            .catch(error => {
+                if (error.response) {
+                    return error.response;
+                  }
+            })
+}
+
+const CheckRoles = async (token) => {
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+    return await api.get('/connect/userinfo');
+}
+
+export default AccountService{
+    LoginService,
+    CheckRoles
+}
